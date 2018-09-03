488bdefc405d0a9d419fdb869b41e7e409391564
SunQ <sunq0313@gmail.com>
2018-09-03 15:53:26 +0900

    add IoU, L1 loss

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,10 @@
 import torch.nn.functional as F
 
 
-def my_loss(y_input, y_target):
-    return F.nll_loss(y_input, y_target)
+def bce_loss(output, target):
+    return F.binary_cross_entropy(output, target)
+
+
+
+def mse_loss(output, target):
+    return F.mse_loss(output, target)
--- model/metric.py
+++ model/metric.py
@@ -1,19 +1,22 @@
+import torch
 import numpy as np
 
 
-def my_metric(y_input, y_target):
-    assert len(y_input) == len(y_target)
-    correct = 0
-    for y0, y1 in zip(y_input, y_target):
-        if np.array_equal(y0, y1):
-            correct += 1
-    return correct / len(y_input)
+def iou_score(output, target, threshold=0.5, eps=1e-8):
+    """ 
+    param 
+        output: binary mask containing model output
+        target: binary mask containing ground truth mask
+    """
+    b, c, w, h = output.shape
+    output = (output > threshold).float()
+    if torch.max(output) == 0:
+        return iou_score(1 - output, 1 - target)
+    output = output.view(b, c*w*h)
+    target = target.view(b, c*w*h)
+
+    intersection = (output * target).sum(1) + eps
+    union = torch.max(output, target).sum(1) + eps
+    return torch.mean(intersection / union)
 
 
-def my_metric2(y_input, y_target):
-    assert len(y_input) == len(y_target)
-    correct = 0
-    for y0, y1 in zip(y_input, y_target):
-        if np.array_equal(y0, y1):
-            correct += 1
-    return correct / len(y_input) * 2

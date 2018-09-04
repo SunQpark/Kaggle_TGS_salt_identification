6d7cd2eff65d9d344b2c883b2d115e88fa79e433
SunQ <sunq0313@gmail.com>
2018-09-04 10:30:46 +0900

    add mean_IoU metric

--- model/metric.py
+++ model/metric.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 
-def iou_score(output, target, threshold=0.5, eps=1e-8):
+def iou_score(output, target, threshold=0.8, eps=1e-8):
     """ 
     param 
         output: binary mask containing model output
@@ -15,8 +15,31 @@ def iou_score(output, target, threshold=0.5, eps=1e-8):
     output = output.view(b, c*w*h)
     target = target.view(b, c*w*h)
 
-    intersection = (output * target).sum(1) + eps
+    intersection = (output * target).sum(1)
     union = torch.max(output, target).sum(1) + eps
+    # print((intersection / union).shape)
     return torch.mean(intersection / union)
 
+def mean_iou(output, target, threshold=0.8, eps=1e-8):
+    b = output.shape[0]
+    output = output.view(b, -1)
+    target = target.view(b, -1).float()
 
+    output = (output > threshold).float()
+    
+    output_empty = torch.max(output, dim=1, keepdim=True)[0] == 0
+    target_empty = torch.max(target, dim=1, keepdim=True)[0] == 0
+    n_TN = (output_empty * target_empty).sum().float()
+
+    intersection = (output * target).sum(1, keepdim=True)
+    union = torch.max(output, target).sum(1) + eps
+    score = intersection / union
+    # print(score)
+    hits = torch.cat([(score > t) for t in np.arange(0.5, 1.0, 0.05)], dim=1).float()
+    prec = torch.sum(hits, dim=0) / (b - n_TN + eps)
+    return torch.mean(prec)
+
+if __name__ == '__main__':
+    out = torch.ones(10, 1, 101, 101) > 0.2
+    tar = torch.ones(10, 1, 101, 101) > 0.3
+    print(mean_iou(out, tar))

c211357338427e0ab392710706a46e93ec914335
SunQ <sunq0313@gmail.com>
2018-09-08 04:07:40 +0900

    add data augmentation

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 import pandas as pd
+import random
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from PIL import Image
@@ -23,22 +24,25 @@ class SaltDataset(Dataset):
     def __len__(self):
         return len(self.metadata.id)
 
-    def _get_image(self, id, mask=False):
+    def _get_image(self, image_id, mask=False):
         mode = 'masks' if mask else 'images'
         img_dir = 'train' if self.train else 'test'
-        img_path = os.path.join(self.data_dir, f'{img_dir}/{mode}/{id}.png')
+        img_path = os.path.join(self.data_dir, f'{img_dir}/{mode}/{image_id}.png')
         return Image.open(img_path)
 
     def __getitem__(self, idx):
         image_id = self.metadata.id[idx]
 
         img  = self._get_image(image_id)
+        seed = np.random.randint(1)
         # apply transforms
+        random.seed(seed)
         if self.transform is not None:
             img = self.transform(img)
         
         if self.train:
             mask = self._get_image(image_id, mask=True)
+            random.seed(seed)
             if self.target_transform is not None:
                 mask = self.target_transform(mask)
                 mask = (mask > 0).float()
@@ -50,6 +54,9 @@ class SaltDataset(Dataset):
 class SaltDataLoader(BaseDataLoader):
     def __init__(self, config):
         trsfm = transforms.Compose([
+            # transforms.ColorJitter(brightness=0.2),
+            transforms.RandomResizedCrop(101, scale=(0.7, 1.0)),# ratio=(0.75, 1.3333333333333333)),
+            transforms.RandomHorizontalFlip(p=0.5),
             transforms.Pad((5, 5, 6, 6), padding_mode='reflect'),
             transforms.ToTensor(),
             ])

6c7c573fb434e89b90ad8d8756daa41d34865a2f
SunQ <sunq0313@gmail.com>
2018-09-30 22:04:07 +0900

    try Lovasz hinge loss

--- model/loss.py
+++ model/loss.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn.functional as F
+from .lovasz_losses import lovasz_hinge
 
 
 def bce_loss(output, target):
@@ -12,4 +14,6 @@ def l1_loss(output, target):
 
 def bce_with_l1_loss(output, target, weight_l1=1.0):
     return (bce_loss(output, target) + weight_l1 * l1_loss(output, target)) / (1.0 + weight_l1)
-    
+
+def lovasz_with_bce(output, target):
+    return (lovasz_hinge(output, target) + bce_loss(output, target) + l1_loss(output, target)) / 3
